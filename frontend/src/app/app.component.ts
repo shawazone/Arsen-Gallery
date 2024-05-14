81abad6a47b1fe1f9d6b70b73a09798bdfa933df
mohamad alshawa <mohamad.alchawwa+github@gmail.com>
2024-05-14 18:17:53 +0300

    working on the landing page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component'; // Import the component
+
 @Component({
   selector: 'app-root',
-  standalone: true, // no need t obe declared a module  
-  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 
-  // u can import templates
-  
 })
 export class AppComponent {
   title = 'frontend';
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+ 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
